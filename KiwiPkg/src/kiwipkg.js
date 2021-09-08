87be7dd1e5565c0c254da0c404ea2113be9f7407
Sundev79 <sundev79@nutdevs.org>
2021-09-08 16:41:14 +0200

    Removed path print (moved to another file)

--- KiwiPkg/src/kiwipkg.js
+++ KiwiPkg/src/kiwipkg.js
@@ -3,9 +3,6 @@
 import KiwiLocalRepo from "./KiwiLocalRepo.js"
 
 let kiwi = new KiwiLocalRepo("E:\\TempWork\\KiwiPkg");
-console.log(kiwi.repoPath)
-
 kiwi.installPkg("https://github.com/NutDevs-org/KiwiPkg.git");
-
 
 export { KiwiLocalRepo };
